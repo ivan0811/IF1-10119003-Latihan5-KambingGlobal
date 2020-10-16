3be764467d8e7473624e3f77968ca0bfb920475d
ivan0811 <ivanfaathirza@gmail.com>
2020-10-16 18:11:32 +0700

    initial commit

---
+++ src/com/kambingglobal/KambingGlobal.java
@@ -0,0 +1,31 @@
+package com.kambingglobal;
+
+/*
+	NAMA = Ivan Faathirza
+	KELAS = IF1
+	NIM = 10119003
+	Deskripsi program = program ini untuk mengakses dan mengubah variabel instance
+	lalu menampilkan ke layar melalui method
+ */
+
+public class KambingGlobal {
+    int jumlahKambing = 88;
+
+    public void getJumlahKambing(){
+        System.out.println("Jumlah kambing: "+jumlahKambing);
+    }
+
+    public void tambahKambing(){
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambahkan: "+jumlahKambing);
+    }
+
+    public static void main(String[] args) {
+        KambingGlobal kambingSusu = new KambingGlobal();
+
+        //Menampilkan jumlah kambing yang ada saat program pertama x perjalan
+        kambingSusu.tambahKambing();
+
+        kambingSusu.getJumlahKambing();
+    }
+}
